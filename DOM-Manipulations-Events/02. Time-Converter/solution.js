ea4ddf4a3d1f89b0d2d271442bad2a24e28a0a98
Viktor <vmakksimov@gmail.com>
2023-09-12 17:50:37 +0300

    DOM Manipulations and Events

--- DOM-Manipulations-Events/02. Time-Converter/solution.js
+++ DOM-Manipulations-Events/02. Time-Converter/solution.js
@@ -1,28 +1,46 @@
 function attachEventsListeners() {
 
-
-    let hours = 24
-    let minutes = 1440
-    let seconds = 866400
+    let options = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
+    }
+    
 
     let daysBtn = document.getElementById('daysBtn')
-    let hoursBtn = document.getElementById('hours')
-    let minutesBtn = document.getElementById('minutes')
-    let secondsBtn = document.getElementById('seconds')
+    let hoursBtn = document.getElementById('hoursBtn')
+    let minutesBtn = document.getElementById('minutesBtn')
+    let secondsBtn = document.getElementById('secondsBtn')
 
+    let days = document.getElementById('days')
+    let hours = document.getElementById('hours')
+    let minutes = document.getElementById('minutes')
+    let seconds= document.getElementById('seconds')
 
     daysBtn.addEventListener('click', onClick)
+    hoursBtn.addEventListener('click', onClick)
+    minutesBtn.addEventListener('click', onClick)
+    secondsBtn.addEventListener('click', onClick)
+
+    function convert(value, unit){
+        let days = value / rotations[unit]
+        return {
+            days: days,
+            hours: days*rotations.hours,
+            minutes: days*rotations.minutes,
+            seconds: days*rotations.seconds
+        }
+    }
 
     function onClick (e){
-        let days =  Number(e.target.parentElement.children[1].value)
-        let current_hours = hours * days
-        let current_minutes = minutes * days
-        let current_seconds = seconds * days
-
-        hoursBtn.value = current_hours
-        minutesBtn.value = current_minutes
-        secondsBtn.value = current_seconds
-
+        let input = e.target.parentElement.querySelector('input[type="text"]')
+        let time = convert(Number(input.value), input.id)
+        days.value = time.days
+        hours.value = time.hours
+        minutes.value = time.minutes
+        seconds.value = time.seconds
+          
 
     }
 
