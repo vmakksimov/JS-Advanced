809cdf4789066dcd0d631f51dd219adb0d51aa0a
Viktor <vmakksimov@gmail.com>
2023-09-12 05:01:16 +0300

    DOM Manipulations and Events

--- DOM-Manipulations-Events/01. Sections/script.js
+++ DOM-Manipulations-Events/01. Sections/script.js
@@ -1,3 +1,16 @@
-function create(words) {
-   console.log('TODO:...');
+function create(input) {
+   let content = document.getElementById('content')
+   for (let i = 0; i < input.length; i++) {
+       let div = document.createElement('div');
+       let p = document.createElement('p');
+       p.textContent = input[i]
+       p.style.display = 'none'
+       div.appendChild(p)
+       div.addEventListener('click', onClick)
+       content.appendChild(div)
+
+       function onClick() {
+           p.style.display = 'block'
+       }
+   }
 }
--- DOM-Manipulations-Events/02. Time-Converter/solution.js
+++ DOM-Manipulations-Events/02. Time-Converter/solution.js
@@ -1,4 +1,30 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+
+    let hours = 24
+    let minutes = 1440
+    let seconds = 866400
+
+    let daysBtn = document.getElementById('daysBtn')
+    let hoursBtn = document.getElementById('hours')
+    let minutesBtn = document.getElementById('minutes')
+    let secondsBtn = document.getElementById('seconds')
+
+
+    daysBtn.addEventListener('click', onClick)
+
+    function onClick (e){
+        let days =  Number(e.target.parentElement.children[1].value)
+        let current_hours = hours * days
+        let current_minutes = minutes * days
+        let current_seconds = seconds * days
+
+        hoursBtn.value = current_hours
+        minutesBtn.value = current_minutes
+        secondsBtn.value = current_seconds
+
+
+    }
+
+
 }
