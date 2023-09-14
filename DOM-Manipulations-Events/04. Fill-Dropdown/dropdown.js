f14510aadbf7df0a440d5cf82e6c9440cfc7e626
Viktor <vmakksimov@gmail.com>
2023-09-14 17:06:13 +0300

    DOM manipulations & events

--- DOM-Manipulations-Events/04. Fill-Dropdown/dropdown.js
+++ DOM-Manipulations-Events/04. Fill-Dropdown/dropdown.js
@@ -2,7 +2,6 @@ function addItem() {
     let itemText= document.getElementById('newItemText')
     let itemValue= document.getElementById('newItemValue')
     let menu = document.getElementById('menu')
-
     let option = document.createElement('option')
 
     option.textContent = itemText.value
@@ -10,7 +9,6 @@ function addItem() {
 
     if (itemText.value.length !== 0 && itemValue.value.length !==0){
         menu.appendChild(option)
-
         itemText.value = ''
         itemValue.value = ''
     }
--- DOM-Manipulations-Events/05. Encode-and-Decode-Messages/app.js
+++ DOM-Manipulations-Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let main = document.getElementById('main')
+    main.children[0].children[2].addEventListener('click', encode)
+    main.children[1].children[2].addEventListener('click', decode)
+    let firstTextArea = main.children[0].children[1]
+    let secondTextArea = main.children[1].children[1]
+
+    function encode() {
+        secondTextArea.value = ''
+        for (let i = 0; i < firstTextArea.value.length; i++) {
+            secondTextArea.value += String.fromCharCode(firstTextArea.value.charCodeAt(i) + 1)
+        }
+        firstTextArea.value = ''
+    }
+
+    function decode (){
+        let decodedWord = ''
+        for (let i = 0; i < secondTextArea.value.length; i++){
+            decodedWord += String.fromCharCode(secondTextArea.value.charCodeAt(i) - 1)
+        }
+
+        secondTextArea.value = decodedWord
+    }
+
+
+
+
 }
