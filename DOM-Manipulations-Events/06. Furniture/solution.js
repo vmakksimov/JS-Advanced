f92f1edf1be0fc076316038598703e45a266caaa
Viktor <vmakksimov@gmail.com>
2023-09-15 20:10:11 +0300

    DOM manipulations & events

--- DOM-Manipulations-Events/06. Furniture/solution.js
+++ DOM-Manipulations-Events/06. Furniture/solution.js
@@ -1,34 +1,45 @@
 function solve() {
-  let div = document.getElementById('exercise')
-  let textArea = div.children[1]
-  let generateButton = div.children[2].addEventListener('click', onGenerate)
-  let table = document.querySelector('.table tbody')
-
-
-  function onGenerate(e){
-      let input = JSON.parse(textArea.value)
-
-
-      for (let i =0; i < input.length; i++){
-        let tr = document.createElement('tr')
-        let entries = Object.entries(input[i])
-        for ([key, value] of entries){
-          let td = document.createElement('td')
-          td.textContent = value
-          tr.appendChild(td)
-        }
-        
-        table.appendChild(tr)
-      }
-     
-
-     
-      
-
-
-  }
-
-
+    let div = document.getElementById('exercise')
+    let textArea = div.children[1]
+    div.children[2].addEventListener('click', onGenerate)
+    let table = document.querySelector('.table tbody')
+    let buyButton = document.querySelectorAll('#exercise button')[1].addEventListener('click', finalButton)
+    let buyArea = document.querySelectorAll('#exercise textarea')[1]
   
-  //TODO...
+   
+
+    function onGenerate(e) {
+        let input = JSON.parse(textArea.value)
+        for (value of input) {
+            table.innerHTML += ` <tr>
+          <td><img src="${value.img}"></td>
+          <td><p>${value.name}</p></td>
+          <td><p>${value.price}</p></td>
+          <td><p>${value.decFactor}</p></td>
+          <td><input type="checkbox"/></td> </tr>`
+
+        }
+    }
+
+    function finalButton(e) {
+        let elementName = []
+        let elementPrice = []
+        let elementFactor = []
+       document.querySelectorAll('input[type=checkbox]').forEach(el => {
+        if (el.checked){
+            elementName.push(el.parentElement.parentElement.children[1].textContent)
+            elementPrice.push(Number(el.parentElement.parentElement.children[2].textContent))
+            elementFactor.push(Number(el.parentElement.parentElement.children[3].textContent))
+        }
+       })
+
+
+       let totalPrice = 0
+       let avgFac = 0
+       elementFactor.map(e => avgFac += e)
+       elementPrice.map(e => totalPrice += e)
+       buyArea.value = `Bought furniture: ${elementName.join(', ')}\nTotal Price: ${totalPrice.toFixed(2)}\n"Average decoration factor: ${avgFac / 2}`
+    }
+
+
 }
