77c165fd67627b7dfdef2c1aceccc9f0505f4db6
Viktor <vmakksimov@gmail.com>
2023-09-16 17:31:56 +0300

    Object & Compositions

--- DOM-Manipulations-Events/06. Furniture/solution.js
+++ DOM-Manipulations-Events/06. Furniture/solution.js
@@ -1,45 +1,48 @@
 function solve() {
     let div = document.getElementById('exercise')
     let textArea = div.children[1]
-    div.children[2].addEventListener('click', onGenerate)
     let table = document.querySelector('.table tbody')
-    let buyButton = document.querySelectorAll('#exercise button')[1].addEventListener('click', finalButton)
-    let buyArea = document.querySelectorAll('#exercise textarea')[1]
-  
-   
+    let buttons = document.querySelectorAll('#exercise button')
+    const generateButton = buttons[0]
+    generateButton.addEventListener('click', onGenerate)
+
+    let textField = document.querySelectorAll('#exercise textarea')
+    let buyArea = textField[1]
 
     function onGenerate(e) {
         let input = JSON.parse(textArea.value)
-        for (value of input) {
+        input.forEach(value => {
             table.innerHTML += ` <tr>
-          <td><img src="${value.img}"></td>
-          <td><p>${value.name}</p></td>
-          <td><p>${value.price}</p></td>
-          <td><p>${value.decFactor}</p></td>
-          <td><input type="checkbox"/></td> </tr>`
+            <td><img src="${value.img}"></td>
+            <td><p>${value.name}</p></td>
+            <td><p>${value.price}</p></td>
+            <td><p>${value.decFactor}</p></td>
+            <td><input type="checkbox"/></td> </tr>`
+        })
 
-        }
     }
+    const buybutton = buttons[1]
+    buybutton.addEventListener('click', finalButton)
 
-    function finalButton(e) {
-        let elementName = []
-        let elementPrice = []
-        let elementFactor = []
-       document.querySelectorAll('input[type=checkbox]').forEach(el => {
-        if (el.checked){
-            elementName.push(el.parentElement.parentElement.children[1].textContent)
-            elementPrice.push(Number(el.parentElement.parentElement.children[2].textContent))
-            elementFactor.push(Number(el.parentElement.parentElement.children[3].textContent))
-        }
-       })
+    function finalButton() {
+        let checking = [...document.querySelectorAll('input:checked')]
+        let names = []
+        let price = 0
+        let factor = 0
+        checking.forEach(element => {
+            console.log(element.parentNode)
+            console.log(element.parentElement)
+            let name = element.parentNode.parentNode.children[1].textContent
+            names.push(name)
+            let price2 = Number(element.parentNode.parentNode.children[2].textContent)
+            price += price2
+            let factor3 = Number(element.parentNode.parentNode.children[3].textContent)
+            factor += factor3
 
-
-       let totalPrice = 0
-       let avgFac = 0
-       elementFactor.map(e => avgFac += e)
-       elementPrice.map(e => totalPrice += e)
-       buyArea.value = `Bought furniture: ${elementName.join(', ')}\nTotal Price: ${totalPrice.toFixed(2)}\n"Average decoration factor: ${avgFac / 2}`
+        });
+    
+        buyArea.innerHTML += `Bought furniture: ${names.join(', ')}\n`
+        buyArea.innerHTML += `Total price: ${price.toFixed(2)}\n`
+        buyArea.innerHTML += `Average decoration factor: ${factor / names.length}`
     }
-
-
 }
--- DOM-Manipulations-Events/09. JavaScript-Quiz/solution.js
+++ DOM-Manipulations-Events/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,46 @@
 function solve() {
-  //TODO...
+    let answers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents']
+    let answersNumber = 0
+    let mainDiv = document.getElementById("quizzie")
+    let results = document.getElementById('results')
+
+    let textResults = results.children[0].children[0]
+    const answer = document.querySelectorAll('.answer-text').forEach(el => {
+        el.addEventListener('click', onClick)
+    })
+
+    let firstSection = mainDiv.children[1]
+    let secondSection = mainDiv.children[2]
+    let thirdSection = mainDiv.children[3]
+
+    function onClick(e) {
+        let currentAnswer = e.target.textContent
+
+        if (answers.includes(currentAnswer)) {
+            if (firstSection.className === '') {
+                firstSection.className = 'hidden'
+                thirdSection.className = 'hidden'
+                secondSection.className = 'show'
+                answersNumber += 1
+            } else if (firstSection.className === 'hidden' && thirdSection.className === 'hidden') {
+                secondSection.className = 'hidden'
+                firstSection.className = 'hidden'
+                thirdSection.className = 'show'
+                answersNumber += 1
+
+            } else if (thirdSection.className === 'show') {
+                textResults.textContent = `You are recognized as top JavaScript fan!`
+                secondSection.className = 'hidden'
+                firstSection.className = 'hidden'
+                thirdSection.className = 'hidden'
+                results.style.display = 'block'
+            }
+        } else {
+            secondSection.className = 'hidden'
+            firstSection.className = 'hidden'
+            thirdSection.className = 'hidden'
+            textResults.textContent = `You have ${answersNumber} right answers.`
+            results.style.display = 'block'
+        }
+    }
 }
