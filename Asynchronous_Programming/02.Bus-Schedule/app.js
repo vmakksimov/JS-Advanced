bfeda26d1d82f9c42c5c926b952199a5de81f8a6
Viktor <vmakksimov@gmail.com>
2023-10-09 01:25:01 +0300

    Asynchronous_programming

--- Asynchronous_Programming/02.Bus-Schedule/app.js
+++ Asynchronous_Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
-
-    function depart() {
-        console.log('Depart TODO...');
+    let url = 'http://localhost:3030/jsonstore/bus/schedule'
+    let span = document.querySelector('.info')
+    let arriveBTN = document.getElementById('arrive')
+    let departBTN = document.getElementById('depart')
+    let stop = {
+        next: 'depot'
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function depart() {
+
+        try {
+            const res = await fetch(`${url}/${stop.next}`)
+            if (res.status !== 200) {
+                throw new Error('Server not found')
+            }
+            stop = await res.json()
+            span.textContent = `Next stop ${stop.name}`
+            departBTN.disabled = true
+            arriveBTN.disabled = false
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    async function arrive() {
+        span.textContent = `Arrived at ${stop.name}`
+        console.log(stop)
+        departBTN.disabled = false
+        arriveBTN.disabled = true
     }
 
     return {
